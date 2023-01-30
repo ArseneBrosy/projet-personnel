4d6e713e97422958a82fb62b6aa257cb0eebf18f
ArseneBrosy <arsene.brosy@divtec.ch>
2023-01-30 17:01:03 +0100

    player stick to wall

--- js/script.js
+++ js/script.js
@@ -11,12 +11,14 @@ canvas.height = window.innerHeight;
 const PLAYER_WIDTH = 50;
 const PLAYER_HEIGHT = 30;
 const PLAYER_SPEED = 10;
+const PLAYER_ROTATE_CHECK_HEIGHT = -15;
+const OUTSIDE_WALL_WIDTH = 30;
 //#endregion
 
 //#region VARIABLES
 //#region PLAYER
 var playerX = canvas.width / 2;
-var playerY = canvas.height - PLAYER_HEIGHT / 2;
+var playerY = canvas.height - PLAYER_HEIGHT / 2 - OUTSIDE_WALL_WIDTH;
 var playerRotation = 0;
 var playerVelocityX = 0;
 //#endregion
@@ -25,7 +27,49 @@ var playerVelocityX = 0;
 var mouseX = 0;
 var mouseY = 0;
 //#endregion
+
+//#region walls
+var walls = []
 //#endregion
+//#endregion
+
+//#region FUNCTIONS
+// check if an x, y coord is in a wall (adjustable margin)
+function isInWall(x, y, margin = 0) {
+    var result = false;
+    for (var i = 0; i < walls.length; i++) {
+        if (x >= walls[i][0] - margin && x <= walls[i][2] + margin && y >= walls[i][1] - margin && y <= walls[i][3] + margin) {
+            result = true;
+        }
+    }
+    return result;
+}
+
+function rotateAround(deg, pos) {
+    if (pos === 0) {
+        var startX = playerX + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+        var startY = playerY - Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+        playerRotation += deg
+        playerX = startX + Math.cos((playerRotation) * (Math.PI/180)) * PLAYER_WIDTH / 2 - Math.cos((90 - playerRotation) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+        playerY = startY + Math.sin((playerRotation) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((90 - playerRotation) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+    }
+    if (pos === 1) {
+        var startX = playerX - Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+        var startY = playerY + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+        playerRotation += deg
+        playerX = startX + Math.cos((playerRotation + 180) * (Math.PI/180)) * PLAYER_WIDTH / 2 - Math.cos((90 - playerRotation + 180) * (Math.PI/180)) * -PLAYER_ROTATE_CHECK_HEIGHT;
+        playerY = startY + Math.sin((playerRotation + 180) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((90 - playerRotation + 180) * (Math.PI/180)) * -PLAYER_ROTATE_CHECK_HEIGHT;
+    }
+}
+//#endregion
+
+// create outside walls
+walls.push([0, 0, OUTSIDE_WALL_WIDTH, canvas.height]);
+walls.push([canvas.width - OUTSIDE_WALL_WIDTH, 0, canvas.width, canvas.height]);
+walls.push([0, 0, canvas.width, OUTSIDE_WALL_WIDTH]);
+walls.push([0, canvas.height - OUTSIDE_WALL_WIDTH, canvas.width, canvas.height]);
+
+walls.push([canvas.width / 2 - OUTSIDE_WALL_WIDTH, 0, canvas.width / 2 + OUTSIDE_WALL_WIDTH, 300])
 
 function loop() {
     // resize canvas
@@ -37,7 +81,43 @@ function loop() {
     playerY -= Math.cos((playerRotation - 90) * (Math.PI/180)) * playerVelocityX;
     //#endregion
 
+    //#region ROTATE PLAYER
+    // left
+    var leftCheckX = playerX + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+    var leftCheckY = playerY - Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+    while (isInWall(leftCheckX, leftCheckY)) {
+        rotateAround(1, 1);
+        leftCheckX = playerX + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+        leftCheckY = playerY - Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+    }
+    while (!isInWall(leftCheckX, leftCheckY)) {
+        rotateAround(-1, 1);
+        leftCheckX = playerX + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+        leftCheckY = playerY - Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+    }
+    // left
+    var rightCheckX = playerX - Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+    var rightCheckY = playerY + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+    while (isInWall(rightCheckX, rightCheckY))  {
+        rotateAround(-1, 0);
+        rightCheckX = playerX - Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+        rightCheckY = playerY + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+    }
+    while (!isInWall(rightCheckX, rightCheckY))  {
+        rotateAround(1, 0);
+        rightCheckX = playerX - Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+        rightCheckY = playerY + Math.cos((playerRotation - 90) * (Math.PI/180)) * PLAYER_WIDTH / 2 + Math.sin((playerRotation - 90) * (Math.PI/180)) * PLAYER_ROTATE_CHECK_HEIGHT;
+    }
+    //#endregion
+
     //#region DRAW
+    // walls
+    for (var i = 0; i < walls.length; i++) {
+        // background
+        ctx.fillStyle = "#E2E289";
+        ctx.fillRect(walls[i][0] - 1, walls[i][1] - 1, walls[i][2] - walls[i][0] + 2, walls[i][3] - walls[i][1] + 2);
+    }
+
     // player
     ctx.fillStyle = "red";
     ctx.translate(playerX, playerY);
